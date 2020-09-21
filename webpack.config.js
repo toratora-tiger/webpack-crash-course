fd2307e9199a0b92ca999d82ef1439a7bdf847df
toratora-tiger <torafusu@gmail.com>
2020-09-22 07:47:08 +0900

    implements sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,13 +18,21 @@ module.exports = {
                 ]
             },
             {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
                 test: /\.(jpe?g|png|gif|svg|ico)$/i,
                 loader: 'url-loader',
                 options: {
                     limit: 2048,
                     name: './images/[name].[ext]'
                 }
-            }
+            },
         ]
     },
     devServer: {
